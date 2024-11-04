981602e88fb338c405e7529c2765244b4d78c3ec
NikolaosPapas <nico.mendivil1@gmail.com>
2024-11-03 16:57:25 -0800

    added js

---
+++ CookieShop/index.js
@@ -0,0 +1,26 @@
+const storeName ="Jaime's Cookie Store"; 
+
+const product1 = "Half Dozen Chocolate Chunk Cookie";
+const product2 = "A Dozen Chocolate Chunk Cookie";
+const product3 = "Half Dozen Italian Butter Cookie";
+const product4 = "A Dozen Italian Butter Cookie";
+const product5 = "Half Dozen Plain Chocolate Cookie";
+const product6 = "A Dozen Plain Chocolate Cookie":
+
+
+const price1 = 12.99;
+const price2 = 19.99;
+const price3 = 13.99;
+const price4 = 22.99;
+const price5 = 9.99;
+const price6 = 14.99;
+
+const storeHours = "Monday - Friday: 9 AM - 6 PM, Saturday: 10 AM - 4 PM, Sunday: Closed";
+
+console.log(storeName, storeType);
+console.log(product1, price1);
+console.log(product2, price2);
+console.log(product3, price3);
+console.log(product4, price4);
+console.log(product5, price5);
+console.log(storeHours);
